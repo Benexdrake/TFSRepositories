0db304d4f70d2440a5c04732145321f39b9fc4b4
BeneX Drake <50136177+Benexdrake@users.noreply.github.com>
2022-12-13 11:43:14 +0100

    Projektdateien hinzufügen.

---
+++ TFSRepositories/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http.Headers;
+async Task GetProjects()
+{
+    try
+    {
+        var personalaccesstoken = "PAT_FROM_WEBSITE";
+
+        using (HttpClient client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                Convert.ToBase64String(
+                    System.Text.ASCIIEncoding.ASCII.GetBytes(
+                        string.Format("{0}:{1}", "", personalaccesstoken))));
+
+            using (HttpResponseMessage response = client.GetAsync(
+                        "https://dev.azure.com/{organization}/_apis/projects").Result)
+            {
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+    }
+}
